4e152eacfdfb8831e3651cf913bf4b188d8cbc03
yocchan-git <yocchan.business@gmail.com>
2024-03-07 14:57:42 +0900

    目標のコメント機能を作成する

---
+++ app/controllers/goals/comments_controller.rb
@@ -0,0 +1,28 @@
+class Goals::CommentsController < ApplicationController
+  before_action :set_goal
+
+  def index
+    @comments = @goal.comments.order(:created_at)
+  end
+
+  def create
+    comment = @goal.comments.build(comment_params)
+    comment.user = current_user
+
+    if comment.save
+      redirect_to goal_comments_path(@goal), notice: 'コメントを投稿しました'
+    else
+      render 'goals/comments/index'
+    end
+  end
+
+  private
+
+  def set_goal
+    @goal = Goal.find(params[:goal_id])
+  end
+
+  def comment_params
+    params.require(:comment).permit(:content)
+  end
+end
--- app/models/goal.rb
+++ app/models/goal.rb
@@ -1,5 +1,6 @@
 class Goal < ApplicationRecord
-  has_many :complete_posts, as: :complete_postable
+  has_many :complete_posts, as: :complete_postable, dependent: :destroy
+  has_many :comments, as: :commentable, dependent: :destroy
   belongs_to :user
 
   validates :title, presence: true, length: { maximum: 256 }
