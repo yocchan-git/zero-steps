4dac70a547f5a218a336ef07f7c65fdaa0df0b68
yocchan-git <yocchan.business@gmail.com>
2024-03-10 14:57:34 +0900

    一覧画面でもフォローができるように修正する

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -2,8 +2,11 @@ class RelationshipsController < ApplicationController
   def create
     @user = User.find(params[:followed_id])
     current_user.follow(@user)
+
+    @is_users_page = params[:is_users_page]
+    redirect_uri = @is_users_page ? users_path : @user
     respond_to do |format|
-      format.html { redirect_to @user }
+      format.html { redirect_to redirect_uri }
       format.turbo_stream
     end
   end
@@ -11,8 +14,12 @@ class RelationshipsController < ApplicationController
   def destroy
     @user = Relationship.find(params[:id]).followed
     current_user.unfollow(@user)
+
+    @is_users_page = params[:is_users_page]
+    redirect_uri = @is_users_page ? users_path : @user
+
     respond_to do |format|
-      format.html { redirect_to @user, status: :see_other }
+      format.html { redirect_to redirect_uri, status: :see_other }
       format.turbo_stream
     end
   end
