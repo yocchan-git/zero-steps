3d48a51bf54e719fde651490d89e5123398d567b
yocchan-git <yocchan.business@gmail.com>
2024-03-11 19:36:30 +0900

    taskコントローラーのリファクタリング

--- app/controllers/goals/tasks_controller.rb
+++ app/controllers/goals/tasks_controller.rb
@@ -1,21 +1,20 @@
 class Goals::TasksController < ApplicationController
+  before_action :set_goal, only: %i[index show]
+  before_action :set_current_user_goal, only: %i[new create edit]
+  before_action :set_task, only: %i[show edit]
+  before_action :set_current_user_task, only: %i[update destroy]
+
   def index
-    @goal = Goal.find(params[:goal_id])
     @tasks = @goal.tasks.order(created_at: :desc).page(params[:page])
   end
 
-  def show
-    @goal = Goal.find(params[:goal_id])
-    @task = @goal.tasks.find(params[:id])
-  end
+  def show; end
 
   def new
-    @goal = current_user.goals.find(params[:goal_id])
     @task = Task.new
   end
 
   def create
-    @goal = current_user.goals.find(params[:goal_id])
     @task = @goal.tasks.build(task_params)
     @task.user = current_user
 
@@ -26,14 +25,9 @@ class Goals::TasksController < ApplicationController
     end
   end
 
-  def edit
-    @goal = current_user.goals.find(params[:goal_id])
-    @task = @goal.tasks.find(params[:id])
-  end
+  def edit; end
 
   def update
-    @task = current_user.tasks.find(params[:id])
-
     if @task.update(task_params)
       redirect_to goal_task_path(@task, goal_id: @task.goal.id), notice: 'タスクを更新しました'
     else
@@ -43,15 +37,29 @@ class Goals::TasksController < ApplicationController
   end
 
   def destroy
-    task = current_user.tasks.find(params[:id])
-    task.destroy!
-
-    redirect_to goal_tasks_path(task.goal), notice: '削除成功しました'
+    @task.destroy!
+    redirect_to goal_tasks_path(@task.goal), notice: '削除成功しました'
   end
 
   private
 
   def task_params
     params.require(:task).permit(:content, :completion_limits)
+  end
+
+  def set_goal
+    @goal = Goal.find(params[:goal_id])
+  end
+
+  def set_task
+    @task = @goal.tasks.find(params[:id])
+  end
+
+  def set_current_user_goal
+    @goal = current_user.goals.find(params[:goal_id])
+  end
+
+  def set_current_user_task
+    @task = current_user.tasks.find(params[:id])
   end
 end
