a7315a5122ba996f36429c308d0b5d67e3faadc8
yocchan-git <yocchan.business@gmail.com>
2024-03-16 12:04:59 +0900

    リダイレクト先をヘルパーメソッドに移行する

--- app/helpers/reactions_helper.rb
+++ app/helpers/reactions_helper.rb
@@ -7,4 +7,9 @@ module ReactionsHelper
       "complete_post_reaction_form#{reactionable.id}"
     end
   end
+
+  # TODO: decoratorメソッドに移動させる
+  def reactionable_uri(reaction)
+    reaction.comment? ? comment_reactions_path(reaction.reactionable) : complete_post_reactions_path(reaction.reactionable)
+  end
 end
