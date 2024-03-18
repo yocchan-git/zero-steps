8519696fae5074a77ff00bdf0d7c16d397ebe90e
yocchan-git <yocchan.business@gmail.com>
2024-03-18 13:04:48 +0900

    コメントの削除を非同期で行う

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,14 @@
 class CommentsController < ApplicationController
+  COMMENT_COUNT = 5
+
   def destroy
     comment = current_user.comments.find(params[:id])
-
     comment.destroy!
-    redirect_to comment.commentable, notice: 'コメントを削除しました'
+
+    @comments = comment.commentable.comments.includes(:user).order(created_at: :desc).page(params[:page]).per(COMMENT_COUNT)
+    respond_to do |format|
+      format.html { redirect_to request.referer }
+      format.turbo_stream
+    end
   end
 end
