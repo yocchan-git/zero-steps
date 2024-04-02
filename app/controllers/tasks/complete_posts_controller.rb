684886efa8fa2da177dec9e266b2cb65747e1579
yocchan-git <yocchan.business@gmail.com>
2024-04-02 17:27:45 +0900

    complete_postコントローラーの修正をする

--- app/controllers/tasks/complete_posts_controller.rb
+++ app/controllers/tasks/complete_posts_controller.rb
@@ -7,14 +7,9 @@ class Tasks::CompletePostsController < ApplicationController
   def new; end
 
   def create
-    @task.completed_at = Time.current
-    @task.save!
+    @task.update!(completed_at: Time.current)
 
-    complete_post = @task.build_complete_post(complete_post_params)
-    complete_post.user = current_user
-
-    if complete_post.save
-      complete_post.timelines.create!(user: current_user, content: "#{current_user.name}さんが#{@task.formatted_content}というタスクを終了しました")
+    if CompletePost.create_with_timelines!(@task, current_user, complete_post_params)
       redirect_to goal_task_path(@task, goal_id: @task.goal.id), notice: '終了投稿しました'
     else
       render 'tasks/complete_posts/new'
--- app/models/complete_post.rb
+++ app/models/complete_post.rb
@@ -11,11 +11,24 @@ class CompletePost < ApplicationRecord
   validates :content, presence: true, length: { maximum: 500 }
   validates :complete_postable_id, uniqueness: { scope: :complete_postable_type }
 
+  def self.create_with_timelines!(complete_postable, user, params)
+    complete_post = complete_postable.build_complete_post(params)
+    complete_post.user = user
+    complete_post.save!
+
+    message = complete_post.goal? ? complete_postable.formatted_title : complete_postable.formatted_content
+    complete_post.timelines.create!(user:, content: "#{user.name}さんが#{message}という#{complete_post.goal? ? '目標' : 'タスク'}を終了しました")
+  end
+
   def complete_post_url
-    if complete_postable_type == 'Goal'
+    if goal?
       goal_path(complete_postable)
     else
       goal_task_path(complete_postable, goal_id: complete_postable.goal.id)
     end
+  end
+
+  def goal?
+    complete_postable_type == 'Goal'
   end
 end
