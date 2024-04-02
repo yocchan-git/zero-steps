76efb7680dc7ccacde0cb8ba61a5a5b7032601a2
yocchan-git <yocchan.business@gmail.com>
2024-04-02 11:21:53 +0900

    Reactionsコントローラのリファクタ

--- app/controllers/complete_posts/reactions_controller.rb
+++ app/controllers/complete_posts/reactions_controller.rb
@@ -3,10 +3,8 @@
 class CompletePosts::ReactionsController < ApplicationController
   def create
     @reactionable = CompletePost.find(params[:complete_post_id])
-    reaction = @reactionable.reactions.build
-    reaction.user = current_user
+    @reactionable.reactions.create!(user: current_user)
 
-    reaction.save!
     respond_to do |format|
       format.html { redirect_back(fallback_location: root_path) }
       format.turbo_stream
