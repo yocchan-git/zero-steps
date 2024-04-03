e80a9dac44588a152cd99433556e58ae255efe37
yocchan-git <yocchan.business@gmail.com>
2024-04-03 14:20:15 +0900

    usersフォルダのリファクタリングする

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 module CommentsHelper
-  def recent_comments(commentable, comment_count:)
-    commentable.comments.eager_load(:user).order(created_at: :desc).limit(comment_count)
+  def recent_comments(resource, comment_count:)
+    resource.comments.preload(:user, :commentable).order(created_at: :desc).limit(comment_count)
   end
 
   def link_text(comments)
